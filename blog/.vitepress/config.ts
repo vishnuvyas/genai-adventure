1c351433231b5967fb9a2795b4e2120018bb2023
vishnu vyas <vishnuvyas@gmail.com>
2023-05-19 23:07:52 -0700

    added more social links

--- blog/.vitepress/config.ts
+++ blog/.vitepress/config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/vishnuvyas/genai-adventure' },
+      { icon: 'twitter', link: 'https://twitter.com/vishnuvyas' }
     ]
   }
 })
